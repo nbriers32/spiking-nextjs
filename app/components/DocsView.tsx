6c14db03d8e58ec0a0af3b6fbc3fdc311d88a6c6
nbriers32 <nathan.briers@tjmorris.co.uk>
2025-05-30 10:37:12 +0100

    moved documents state up to docs page

--- app/components/DocsView.tsx
+++ app/components/DocsView.tsx
@@ -1,35 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 interface Document {
     name: string,
     type: string
     version: number,
 }
+interface DocsViewProps {
+    documentsArr: Document[],
+    loading: boolean,
+    error: string | null,
+}
 
-const DocsView = () => {
-    const [documents, setDocuments] = useState<Document[]>([])
-    const [loading, setLoading] = useState(true)
-    const [error , setError] = useState(false)
-
-    useEffect(() => {
-        const getDocumentsData = async () => {
-            try {
-                const req = await fetch("/api/docs")
-                const documents = await req.json()
-                setDocuments(documents.data as Document[])
-            } catch(err: any) {
-                setError(err.message)
-            } finally {setLoading(false)}
-        }
-        getDocumentsData()
-    }, [])
+const DocsView: React.FC<DocsViewProps> = ({documentsArr, loading, error}) => {
     return (
         <>
             { error ? <p> Error getting documents </p> 
             : loading ? <p> Loading documents </p> 
             : ( 
-                <div className="grid sm: grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-                {documents.map((document, i) => {
+                <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
+                {documentsArr.map((document, i) => {
                     return (
                         <div key={i} className="bg-white dark:bg-gray-800 rounded-3xl p-4 shadow-xl">
                         <span className="flex flex-row">
--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,14 +1,41 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DocsView from '../components/DocsView'
-
+interface Document {
+    name: string,
+    type: string
+    version: number,
+}
 
 const DocsPage = () => {
+    const [documentsArr, setDocumentsArr] = useState<Document[]>([])
+    const [loading, setLoading] = useState(true)
+    const [error , setError] = useState("")
+
+    useEffect(() => {
+        const getDocumentsData = async () => {
+            try {
+                const res = await fetch("/api/docs")
+                const documents = await res.json()
+                setDocumentsArr(documents.data as Document[])
+            } catch(err: any) {
+                setError(err.message)
+            } finally {setLoading(false)}
+        }
+        getDocumentsData()
+    }, [])
+
     return (
         <>
-        <h2 className="font-bold text-3xl"> Documents </h2>
+        <div className="flex justify-between max-sm:flex-col"> 
+            <h2 className="font-bold text-3xl"> Documents </h2>
+            <div className="flex items-center gap-4">
+                <button className="p-2 bg-blue-700 text-white font-bold rounded shadow"> Create New </button>
+                <button className="p-2 bg-white font-bold rounded shadow"> Filter By Type </button>
+            </div>
+        </div>
         <br/>
-        <DocsView/>
+        <DocsView documentsArr={documentsArr} loading={loading} error={error}/>
         </>
     )
 }
